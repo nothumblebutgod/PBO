f513796f4f095801e4507ab3d87e34afebfd1eb7
nothumblebutgod <100134567+nothumblebutgod@users.noreply.github.com>
2023-02-22 19:43:55 +0700

    Add files via upload

---
+++ Titik.java
@@ -0,0 +1,30 @@
+class Titik{
+	double absis;
+	double ordinat;
+	static int counterTitik; //menghitung objek titik
+	Titik(double a, double o){
+		counterTitik++;
+		absis = a;
+		ordinat = o;
+	}
+	Titik(){
+		counterTitik++;
+		absis = 0;
+		ordinat = 0;
+	}
+	void setAbsis(double a){
+		absis = a;
+	}
+	void setOrdinat(double o){
+		ordinat = o;
+	}
+	double getAbsis(){
+		return absis;
+	}
+	double getOrdinat(){
+		return ordinat;
+	}
+	int getCounterTitik(){
+		return counterTitik;
+	}
+}
