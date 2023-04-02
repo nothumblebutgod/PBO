609df32019c67cf24a8bff98083afc656603d634
nothumblebutgod <100134567+nothumblebutgod@users.noreply.github.com>
2023-04-02 23:50:49 +0700

    Update AngkaSialException.java

--- Pertemuan 6/AngkaSialException.java
+++ Pertemuan 6/AngkaSialException.java
@@ -1,3 +1,7 @@
+//Nama : Ananda Rizky Pratama
+//NIM : 24060121140118
+//LAB : C1
+
 public class AngkaSialException extends Exception{
 	public AngkaSialException(){
 		super("jangan memasukkan angka 13 karena angka sial!!!");
